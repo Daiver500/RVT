9657006024dddb54672e5e4773d061f3dc64b559
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-05-26 09:54:29 +0300

    classes

--- source/js/main.js
+++ source/js/main.js
@@ -39,15 +39,47 @@ const checkboxClickHandler = () => {
 
 checkbobxInput.addEventListener("change", checkboxClickHandler);
 
-let result = document.querySelector(".calculator__rate");
-result.textContent = 111;
 
 
 const MIN_WC_SQUARE = 1;
 const MAX_WC_SQUARE = 20;
 const MIN_APPART_SQUARE = 1;
 const MAX_APPART_SQUARE = 300;
+const NEW__APP__VALUE = 8000;
+const OLD__APP_VALUE = 6500;
+const ELECTRICITY__YES = 300;
+const ELECTRICITY__NO = 600;
+const COSMETIC_REPAIR = 0;
+const FULL_REPAIR = 1500;
+const DESIGN__REPAIR = 3000;
+const WC__REPAIR__YES = 13000;
+const DOORS__YES = 8500;
+const HEATED__FLOOR__YES = 200;
+
+const typeOfBuilding = document.getElementById("type-of-building");
+let squareOfBuilding = document.getElementById("square-of-building");
+let electricityCost = document.getElementById("electricity");
+let result = document.querySelector(".calculator__rate");
 
 
+
+typeOfBuilding.addEventListener("change", getBuildingPrice);
+electricityCost.addEventListener("change", getElectricityPrice);
+
+
+
+
+const squareInputHandler = () => {
+  const valueLength = squareOfBuilding.value;
+  if (valueLength > MAX_APPART_SQUARE) {
+    squareOfBuilding.value = MAX_APPART_SQUARE;
+    squareOfBuilding.setCustomValidity(`Площадь квартиры от 1 до 300 м.`);
+    } else {
+    squareOfBuilding.setCustomValidity(``);
+  }
+  squareOfBuilding.reportValidity();
+};
+
+squareOfBuilding.addEventListener("input", squareInputHandler);
 
 })();
